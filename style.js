34349cd99a8cc0f06d4860d34d150c0406b0b333
donchris247 <kasiechris247@gmail.com>
2023-04-04 12:08:48 +0100

    initial commit

---
+++ style.js
@@ -0,0 +1,11 @@
+function change1() {
+    document.getElementById('motor').src="./images/1.png";
+}
+
+function change2() {
+    document.getElementById('motor').src="./images/2.png";
+}
+
+function change3() {
+    document.getElementById('motor').src="./images/3.png";
+}
